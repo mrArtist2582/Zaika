fb91552451308d90897297dcbc5ab4203955a073
Kashish darji <139367306+mrArtist2582@users.noreply.github.com>
2025-02-15 11:25:15 +0530

    Update dark_mode.dart

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 final ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
-    surface: Color(0xFF181818), // Dark background
-    primary: Color(0xFFFFA825), // Swiggy’s signature orange
-    secondary: Color(0xFF222222), // Dark grey for subtle contrast
-    tertiary: Color(0xFF303030), // Medium grey for cards, containers
-    inversePrimary: Colors.grey.shade300, // Light text/icons for contrast
+    surface: Color(0xFF181818), 
+    primary: Color(0xFFFFA825), 
+    secondary: Color(0xFF222222), 
+    tertiary: Color(0xFF303030), 
+    inversePrimary: Colors.grey.shade300, 
   ),
 );
